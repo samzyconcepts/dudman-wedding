921f1201ef13cea038198aa88146ca76098a94b2
samzyconcepts <samzyolufemi@gmail.com>
2023-03-21 11:52:13 +0100

    update nav and landing page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
 
       <section id='info'>
         <div className='container'>
-          <div>
+          <div className='rsvp'>
             <h3>
               We're really excited to see you, and hope you can join us for our
               special day!
@@ -35,13 +35,13 @@ const Home = () => {
               RSVP
             </Link>
           </div>
-          <div>
+          <div className='detail'>
             <div className='when'>
               <span>WHEN</span>
               <p>Thursday, April 20, 2023 @ 7:00am</p>
-              <span>
+              <p>
                 Engagement, Church joining and Reception follows at the Gallery
-              </span>
+              </p>
             </div>
             <div className='where'>
               <span>WHERE</span>
@@ -51,7 +51,56 @@ const Home = () => {
               </p>
               <Link to='#'>Get Directions</Link>
             </div>
+            <div className='dress'>
+              <span>DRESS CODE</span>
+              <p>White, Purple and Magenta</p>
+            </div>
           </div>
+        </div>
+      </section>
+
+      <section id='story'>
+        <div className='container'>
+          <h3>A BRIEF HISTORY OF LOVE</h3>
+          <p>
+            Oluwaseun and I met during my ND days in 2013. He just handed over
+            as an exco in my campus fellowship CGACCF (Christ Gospel Apostolic
+            Campus Fellowship). We hardly talk🤷🏿‍♀️ wetin I wan dey follow MOG a
+            whole BABA DRAMA talk🤣. It was just greeting, no exchange of
+            contacts. <br />
+            Fast forward to 2015. I sent him msg on facebook saying "Ogami
+            Sir"(I can't disrespect man of God now🤣🤣) and he replied with
+            "Sister Timi Of God" 🤣🤣🤣. <br />
+            We started chatting, talked at length so I told him about my fashion
+            designing work and he promised to snap me for FREE, also helped with
+            social media contents and extra tips to grow my fashion designing
+            business (Very generous right🤭😊) We were friends for a while
+            before I agreed to be his girlfriend in 2016. <br />
+            N:B- Baba toasted me low-key😉 I told him I am his sister and he
+            shouldn't think of such again. 🤷🏿‍♀️ baba was shy to talk to me again
+            for few days. He later sent his friend emmanuel to me🤣🤣🤣😫And me
+            as the hard girl!hard girl I sent emmy back and told him to tell dud
+            to come back by himself this was in March 2016. Baba didn't say
+            anything for a while o (I guess he was practicing how he would
+            express himself🤣🤣🤪🤪). <br />
+            To cut the long story short. April 9, 2016 I agreed to be his
+            girlfriend (drumrolls💃) I said girlfriend "Not Wife"🤣🤣we didn't
+            see each other through out a year ( It was a long distance
+            relationship😫🤦🏿‍️) . <br />
+            We finally saw each other in 2017 when we gained admission for our
+            HND. While in school it wasn't smooth but this guy pulled me
+            through! We had to struggle together to pay our school fees! Gather
+            my friends to snap passport from him just to sustain ourselves on
+            campus. I respect this guy on how he balanced his duty as an exco
+            again in the fellowship with his side hustle (He's so dedicated and
+            good at what he does 🙌🏿🙌🏿). <br />
+            7yrs ago, I started this beautiful journey with my DUD and now we
+            are taking it to forever💕💕..... <br />
+            STIMMY Said Yes To DUD <br /> <br />
+            #Sti'Dud <br />
+            #Dud'Sti In Love <br />
+            #Thefashiondesignerandphotographerlovestory
+          </p>
         </div>
       </section>
     </>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,34 +1,34 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import './Navbar.css'
+import './Navbar.css';
 function Navbar() {
   const [toggleState, setToggleState] = useState(false);
 
-  const handleClick = ()=>{
-    setToggleState(toggleState => !toggleState)
-  }
+  const handleClick = () => {
+    setToggleState((toggleState) => !toggleState);
+  };
 
   let toggleLinkCheck = toggleState ? ' active' : '';
 
   return (
     <nav className='nav'>
-      <div className="container-lg">
-          <Link to='/'><h3>Oluwaseun&Oluwatimilehin</h3></Link>
-          
-          <i 
-          onClick={handleClick}
-          className='bx bx-menu'></i>
-          <div className={`links${toggleLinkCheck}`}>
-            <Link to='#info'>Info & RSVP</Link>
-            <Link to='#story'>Our Story</Link>
-            <Link to='#party'>Party</Link>
-            <Link to='#gallery'>Gallery</Link>
-            <i className='bx bx-x' onClick={handleClick} ></i>
-          </div>
+      <div className='container-lg'>
+        <Link to='/'>
+          <h3>Oluwaseun&Oluwatimilehin</h3>
+        </Link>
+
+        <i onClick={handleClick} className='bx bx-menu'></i>
+        <div className={`links${toggleLinkCheck}`}>
+          <Link to='#info'>Info & RSVP</Link>
+          <Link to='#story'>Our Story</Link>
+          <Link to='#party'>Party</Link>
+          <Link to='#gallery'>Gallery</Link>
+          <i className='bx bx-x' onClick={handleClick}></i>
+        </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

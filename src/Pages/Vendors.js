72bf9da6d62d72adf8db70fdba80e3c495050928
samzyconcepts <samzyolufemi@gmail.com>
2023-04-14 21:40:09 +0100

    add vendor page

---
+++ src/Pages/Vendors.js
@@ -0,0 +1,88 @@
+// css
+import './Vendors.css';
+
+const Vendors = () => {
+  return (
+    <div className='vendor'>
+      <div className='outfit'>
+        <p>
+          <span className='title'>Grooms agbada:</span> Frosh_fashion
+        </p>
+        <p>
+          <span className='title'>Grooms stylist:</span> Jakes Signature
+        </p>
+        <p>
+          <span className='title'>Bride wedding gown:</span> Stimmy_stitches
+        </p>
+        <p>
+          <span className='title'>bride native attire:</span> Yinkus fashion
+        </p>
+        <p>
+          <span className='title'>bride shoe:</span> House of carodavi
+        </p>
+        <p>
+          <span className='title'>grooms shoe:</span> Engine_footwear
+        </p>
+      </div>
+      <div className='visual'>
+        <h2>Visuals Squad</h2>
+        <p>Tuntop Photography</p>
+        <p>Yusog Photography</p>
+        <p>ODM Studios</p>
+        <p>Cloud 3 studios</p>
+        <p>Nobulfilmworks (Videography)</p>
+        <p>Vision Multimedia (Livestream)</p>
+        <p>FMD Geo services (Drone)</p>
+      </div>
+      <div className='decor'>
+        <p>
+          <span className='title'>ring:</span> Nimbjewelries_collections
+        </p>
+        <p>
+          <span className='title'>decoration:</span> borahniel_events Alaga
+        </p>
+      </div>
+      <div className='decor'>
+        <p>
+          <span className='title'>Cake:</span> Softcreamycakes
+        </p>
+        <p>
+          <span className='title'>Bride's Makeup:</span> Kourtneybronzenbrides
+        </p>
+        <p>
+          <span className='title'>Asoebi Outfits:</span> Fabrics_by_ibidunni
+        </p>
+        <p>
+          <span className='title'>Bridal Train make up:</span> Aduke_glam
+        </p>
+        <p>
+          <span className='title'>Bridal Train Outfits:</span> Nikkgold Empire
+        </p>
+        <p>
+          <span className='title'>Bridal Train Fascinator & Flowers:</span>{' '}
+          Dakemar Bridal
+        </p>
+        <p>
+          <span className='title'>HairStylist:</span> Styledbyimmaculate
+        </p>
+        <p>
+          <span className='title'>Souvenirs:</span> Stimmy'shomeofsouvenirs
+        </p>
+        <p>
+          <span className='title'> Mc:</span> Mc Skelebe
+        </p>
+        <p>
+          <span className='title'>DJ:</span> Dj_tez
+        </p>
+        <p>
+          <span className='title'>Band:</span> Olukunle Sunday
+        </p>
+        <p>
+          <span className='title'>Website development:</span> Samzy
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Vendors;
